0ca5cbd5a45d2f4934e0eeb8b7603cdaa63f512e
hadeer <hadeeressam21@gmail.com>
2025-08-05 17:05:49 +0300

    $ add the name of dessert item within data

--- app/src/main/java/com/hadeer/dessetpusher/Data.kt
+++ app/src/main/java/com/hadeer/dessetpusher/Data.kt
@@ -2,18 +2,18 @@ package com.hadeer.dessetpusher
 
 object Data {
     val DessertData = listOf(
-        Dessert(1,R.drawable.cupcake, 5),
-        Dessert(2,R.drawable.donut, 10),
-        Dessert(3,R.drawable.eclair, 15),
-        Dessert(4,R.drawable.froyo, 30),
-        Dessert(5,R.drawable.gingerbread, 50),
-        Dessert(6,R.drawable.honeycomb, 100),
-        Dessert(7,R.drawable.icecreamsandwich, 500),
-        Dessert(8,R.drawable.jellybean, 1000),
-        Dessert(9,R.drawable.kitkat, 2000),
-        Dessert(10,R.drawable.lollipop, 3000),
-        Dessert(11,R.drawable.marshmallow, 4000),
-        Dessert(12,R.drawable.nougat, 5000),
-        Dessert(13,R.drawable.oreo, 6000)
+        Dessert(1,R.drawable.cupcake, 5, "cup cake"),
+        Dessert(2,R.drawable.donut, 10 , "donut"),
+        Dessert(3,R.drawable.eclair, 15 , "eclair"),
+        Dessert(4,R.drawable.froyo, 30, "froyo"),
+        Dessert(5,R.drawable.gingerbread, 50, "ginger bread"),
+        Dessert(6,R.drawable.honeycomb, 100 , "honey comb"),
+        Dessert(7,R.drawable.icecreamsandwich, 500 , "ice-cream sandwich"),
+        Dessert(8,R.drawable.jellybean, 1000, "jelly bean"),
+        Dessert(9,R.drawable.kitkat, 2000, "kit kat"),
+        Dessert(10,R.drawable.lollipop, 3000, "lollipop"),
+        Dessert(11,R.drawable.marshmallow, 4000, "marshmallow"),
+        Dessert(12,R.drawable.nougat, 5000 , "nougat"),
+        Dessert(13,R.drawable.oreo, 6000 , "oreo")
     )
 }
--- app/src/main/java/com/hadeer/dessetpusher/DessertData.kt
+++ app/src/main/java/com/hadeer/dessetpusher/DessertData.kt
@@ -3,5 +3,6 @@ package com.hadeer.dessetpusher
 data class Dessert(
     val id : Int,
     val image : Int,
-    val price : Int
+    val price : Int,
+    val name : String
 )
